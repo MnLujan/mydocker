c46b8d5e60a73f583e57561b1737ff0b5803bb70
Martin Lujan <mlujan@zeus.localdomain>
2021-03-28 02:18:13 -0300

    update certbot.sh

--- certbot/certbot.sh
+++ certbot/certbot.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/bash
 TEMPfile="/tmp/cert_cron.txt"
-/bin/echo "[Proxy] Renovacion de certificados: " > "${TEMPfile}"
-/usr/local/bin/certbot certonly --dry-run --noninteractive --force-renewal --webroot -w /etc/letsencrypt/ -d floyd.voipgroup.com >> "${TEMPfile}" 2>&1
+/bin/echo "[Centrex5] Renovacion de certificados: " > "${TEMPfile}"
 
-/bin/echo "[Proxy] Nginx restart: " >> "${TEMPfile}"
+/usr/bin/certbot certonly --dry-run --noninteractive --force-renewal --webroot -w /etc/letsencrypt/ -d floyd.voipgroup.com >> "${TEMPfile}" 2>&1
+
+/bin/echo "[Centrex5]" >> "${TEMPfile}"
 
 #/usr/sbin/service nginx restart >> "${TEMPfile}" 2>&1
 
 #####################################
 #	Mail config
 #####################################
-SERVER="pegaso.psi.unc.edu.ar"
-FROM="lev2@fcefyn.unc.edu.ar"
-TO="lc.fcefyn@gmail.com"
-SUBJ="[Proxy-homer][AUTOMATICO]Renovacion de Certificados"
+SERVER="mail.voipgroup.com"
+FROM="dev@voipgroup.com"
+TO="martinlujan.ic@gmail.com"
+SUBJ="[Centrex5][AUTOMATICO]Renovacion de Certificados"
 MESSAGE="$(cat ${TEMPfile})"
 CHARSET="utf-8"
 
-/bin/echo ${/usr/bin/ls -l /etc/letsencrypt/}
+echo $(cat ${TEMPfile})
+
+/bin/echo certs: $(ls -l /etc/letsencrypt/)
 
 #/usr/local/bin/sendEmail -f $FROM -t $TO -u $SUBJ -s $SERVER -m "${MESSAGE}" -v -o message-charset=$CHARSET
 
