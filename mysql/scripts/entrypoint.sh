00cf713caa8987c5a8df7d5cf9434e9a49f664da
MnLujan <mnlujan@voipgroup.com>
2021-04-19 11:48:49 -0300

    add changes mysql image

--- mysql/scripts/entrypoint.sh
+++ mysql/scripts/entrypoint.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-echo "[Entrypoint] Config MySQL DataBase"
+echo "[`date`][Entrypoint] Starting MySQL DataBase"
 
-pass="*Q1w2e3"
-
-echo "[Entrypoint] Starting MySQL DataBase"
+echo "[`date`][Entrypoint] Config MySQL DataBase"
 
 service mysql stop
+
 usermod -d /var/lib/mysql/ mysql
+
 service mysql start
 
-cd /home/voipgroup
+cd /home/database
 
-mysqladmin -u root password $pass
+echo "[`date`][Entrypoint] Restore Backup"
 mysql --user="root" --execute="CREATE DATABASE newcentrex"
 mysql -u root newcentrex < backup.sql
+echo "[`date`][Entrypoint] Creater User"
+mysql --user="root" --execute="GRANT ALL PRIVILEGES ON newcentrex.* TO 'centrex'@'127.0.0.1' IDENTIFIED BY 'centrex123' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql --user="root" --execute="GRANT ALL PRIVILEGES ON newcentrex.* TO 'centrex'@'localhost' IDENTIFIED BY 'centrex123' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql --user="root" --execute="GRANT ALL PRIVILEGES ON newcentrex.* TO 'centrex'@'%.%.%.%' IDENTIFIED BY 'centrex123' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql --user="root" --execute="GRANT ALL PRIVILEGES ON newcentrex.* TO 'DBAcentrex'@'%.%.%.%' IDENTIFIED BY 'DBAcentrex123' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql --user="root" --execute="GRANT ALL PRIVILEGES ON newcentrex.* TO 'root'@'%.%.%.%' IDENTIFIED BY 'DBAcentrex123' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql --user="root" --execute="GRANT ALL PRIVILEGES ON newcentrex.* TO 'root'@'127.0.0.1' IDENTIFIED BY 'DBAcentrex123' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
-mysql --user="root" --password="$pass" --execute="CREATE USER 'DBAcentrex'@'%.%.%.%' IDENTIFIED BY 'DBAcentrex123'; GRANT ALL PRIVILEGES ON *.* TO 'DBAcentrex'@'%.%.%.%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-
-echo "[Entrypoint] Restore Backup"
+echo "[`date`][Entrypoint] BD ready !"
 
 /bin/bash
