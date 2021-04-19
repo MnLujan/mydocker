840f9055ad6aa4f94a11b6e64409b15e3e48fb03
MnLujan <martinlujan96@gmail.com>
2021-04-19 13:27:56 -0300

    fix mysql and asterisk-nowebrtc

--- mysql/scripts/entrypoint.sh
+++ mysql/scripts/entrypoint.sh
@@ -3,20 +3,21 @@ echo "[`date`][Entrypoint] Starting MySQL DataBase"
 
 echo "[`date`][Entrypoint] Config MySQL DataBase"
 
-cd /home/database
+cd /
 
 service mysql start
 
 echo "[`date`][Entrypoint] Restore Backup"
-mysql --user="root" --execute="CREATE DATABASE newcentrex"
+
+mysql --user="root" --execute="CREATE DATABASE newcentrex;"
 mysql -u root newcentrex < backup.sql
+
 echo "[`date`][Entrypoint] Creater User"
-mysql --user="root" --execute="GRANT ALL PRIVILEGES ON newcentrex.* TO 'centrex'@'127.0.0.1' IDENTIFIED BY 'centrex123' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-mysql --user="root" --execute="GRANT ALL PRIVILEGES ON newcentrex.* TO 'centrex'@'localhost' IDENTIFIED BY 'centrex123' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-mysql --user="root" --execute="GRANT ALL PRIVILEGES ON newcentrex.* TO 'centrex'@'%.%.%.%' IDENTIFIED BY 'centrex123' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-mysql --user="root" --execute="GRANT ALL PRIVILEGES ON newcentrex.* TO 'DBAcentrex'@'%.%.%.%' IDENTIFIED BY 'DBAcentrex123' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-mysql --user="root" --execute="GRANT ALL PRIVILEGES ON newcentrex.* TO 'root'@'%.%.%.%' IDENTIFIED BY 'DBAcentrex123' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-mysql --user="root" --execute="GRANT ALL PRIVILEGES ON newcentrex.* TO 'root'@'127.0.0.1' IDENTIFIED BY 'DBAcentrex123' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
+mysql --user="root" --execute="CREATE USER 'centrex'@'127.0.0.1' IDENTIFIED BY 'centrex123'; GRANT ALL PRIVILEGES ON newcentrex.* TO 'centrex'@'127.0.0.1' WITH GRANT OPTION;
+CREATE USER 'centrex'@'%.%.%.%' IDENTIFIED BY 'centrex123'; GRANT ALL PRIVILEGES ON newcentrex.* TO 'centrex'@'%.%.%.%' WITH GRANT OPTION;
+CREATE USER 'DBAcentrex'@'%.%.%.%' IDENTIFIED BY 'DBAcentrex123'; GRANT ALL PRIVILEGES ON *.* TO 'DBAcentrex'@'%.%.%.%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;"
 
 echo "[`date`][Entrypoint] BD ready !"
 
